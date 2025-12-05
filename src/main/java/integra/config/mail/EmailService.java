441324d70bf7025589168f6dc26a18879fe94ede
exon Reyes <p.reyes1704@gmail.com>
2025-12-05 10:13:28 -0600

    Se agrega nombre personal Integra para estrategia visual

--- src/main/java/integra/config/mail/EmailService.java
+++ src/main/java/integra/config/mail/EmailService.java
@@ -1,28 +1,24 @@
 package integra.config.mail;
 
 import jakarta.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import lombok.AllArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-// Si querés soporte MIME (HTML / adjuntos), también importar MimeMessage, MimeMessageHelper
 
 @Service
+@AllArgsConstructor
 public class EmailService {
-
-    @Autowired
     private JavaMailSender mailSender;
-
     public void sendHtmlEmail(String to, String subject, String htmlBody) throws Exception {
         MimeMessage message = mailSender.createMimeMessage();
         // true = multipart, necesario si luego quieres adjuntos o imágenes inline
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
         helper.setTo(to);
+        helper.setFrom("sci.integra@gmail.com", "Integra");
         helper.setSubject(subject);
         helper.setText(htmlBody, true);   // true = body es HTML
-
         mailSender.send(message);
     }
 }
